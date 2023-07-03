1ab57cdaa778d152f77b94c545fccf1307bd0ad4
Akshay <akshay@olvy.co>
2023-07-03 11:34:49 +0530

    add - getOlvyUtils, scriptLoader to component

--- src/components/OlvyWidget.vue
+++ src/components/OlvyWidget.vue
@@ -15,8 +15,39 @@ export default {
         }
     },
     methods: {
-        showWidget(widgetAlias, widgetAliasOrID) {
-            window.OlvyUtils.showWidget(widgetAlias, widgetAliasOrID)
+        async getOlvyUtils(){
+            if (window.OlvyUtils) {
+                return window.OlvyUtils;
+            } else {
+                // Wait for the script to load before returning the utils object
+                await this.olvyScriptLoader();
+
+                // script adds utils to window, we just return utils after ensuring script load
+                return window.OlvyUtils;
+            }
+        },
+        async olvyScriptLoader(){
+            // Create a Promise that resolves when the script is loaded
+            return new Promise((resolve, reject) => {
+                if (!window.OlvyUtils) {
+                    const script = document.createElement("script");
+                    script.src = "https://app.olvy.co/scriptV2.js";
+                    script.onload = () => {
+                        // The script is loaded, so resolve the Promise
+                        resolve(true);
+                    };
+                    script.onerror = () => {
+                        // There was an error loading the script, so reject the Promise
+                        reject(new Error("Failed to load script."));
+                    };
+                    document.body.appendChild(script);
+                } else {
+                    resolve(true);
+                }
+            });
+        },
+        showWidget(workspaceAlias, widgetAliasOrID) {
+            window.OlvyUtils.showWidget(workspaceAlias, widgetAliasOrID)
         },
         hideWidget(workspaceAlias, widgetAliasOrID) {
             window.OlvyUtils.hideWidget(workspaceAlias, widgetAliasOrID);
@@ -58,19 +89,26 @@ export default {
             window.OlvyUtils.createFeedback(workspaceAlias, params);
         },
     },
-    mounted() {
+    async mounted() {
         try {
-            if (document) {
-                let olvyScript = document.createElement('script')
-                olvyScript.setAttribute('src', 'https://app.olvy.co/scriptV2.js')
-                document.head.appendChild(olvyScript)
+            if(window) {
+                await this.getOlvyUtils();
+                if(!window.OlvyInstances.length && this.config.workspaceAlias) {
+                    // if no olvyInstances found, we create using config
+                    new window.Olvy(this.config.workspaceAlias);
+                }
             }
-            if (window) {
-                window.addEventListener('load', () => {
-                    window.OlvyConfig = this.config
-                    this.olvyUtils = window.OlvyUtils
-                })
-            }
+            // if (document) {
+            //     let olvyScript = document.createElement('script')
+            //     olvyScript.setAttribute('src', 'https://app.olvy.co/scriptV2.js')
+            //     document.head.appendChild(olvyScript)
+            // }
+            // if (window) {
+            //     window.addEventListener('load', () => {
+            //         window.OlvyConfig = this.config
+            //         this.olvyUtils = window.OlvyUtils
+            //     })
+            // }
         } catch (e) {
             console.log(e)
         }
--- src/install.js
+++ src/install.js
@@ -2,7 +2,11 @@ import OlvyWidget from "./components/OlvyWidget.vue"
 
 const olvyWidget = {
     install (Vue) {
+      // adds component to vue instance
       Vue.component("olvy-widget", OlvyWidget)
+
+      // need something for nuxt
+      
     }
   }
 export default olvyWidget
