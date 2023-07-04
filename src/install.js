bdccddfc09987d7f48fd98c0976be544dd3264ab
Akshay <akshay@olvy.co>
2023-07-04 17:33:09 +0530

    format - prettier

--- src/install.js
+++ src/install.js
@@ -2,17 +2,17 @@ import OlvyWidget from "./components/OlvyWidget.vue"
 import Vue from "vue";
 
 const olvyWidget = {
-    install (appInstance) {
-      if(appInstance.globalName == 'nuxt' && appInstance.vueApp) {
-        console.log(".... nuxt instance.. ", appInstance);
-        appInstance.vueApp.use(OlvyWidget, "OlvyWidget");
-      } else {
-        console.log("... vue instance..", Vue);
-        // adds component to vue instance
-        Vue.component("olvy-widget", OlvyWidget)
-      }
-      
+  install (appInstance) {
+    if(appInstance.globalName == 'nuxt' && appInstance.vueApp) {
+      console.log(".... nuxt instance.. ", appInstance);
+      appInstance.vueApp.use(OlvyWidget, "OlvyWidget");
+    } else {
+      console.log("... vue instance..", Vue);
+      // adds component to vue instance
+      Vue.component("olvy-widget", OlvyWidget)
     }
+    
   }
+}
 export default olvyWidget
 
