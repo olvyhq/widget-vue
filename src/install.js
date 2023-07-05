bac314e08f521583150ce6782c0911536487a81b
Akshay <akshay@olvy.co>
2023-07-05 10:33:36 +0530

    add - changes to exports from install.js

--- src/install.js
+++ src/install.js
@@ -13,17 +13,16 @@ import OlvyWidget from "./components/OlvyWidget.vue"
 // }
 // export default olvyWidget
 
-export default {
-  install (appInstance) {
-    if(appInstance.globalName == 'nuxt' && appInstance.vueApp) {
-      console.log(".... nuxt instance.. ", appInstance);
-      appInstance.vueApp.use(OlvyWidget, "OlvyWidget");
-    } else {
-      // adds component to vue instance
-      appInstance.component("olvy-widget", OlvyWidget)
-    }
+const install = (appInstance) => {
+  if(appInstance.globalName == 'nuxt' && appInstance.vueApp) {
+    console.log(".... nuxt instance.. ", appInstance);
+    appInstance.vueApp.use(OlvyWidget, "OlvyWidget");
+  } else {
+    // adds component to vue instance
+    appInstance.component("olvy-widget", OlvyWidget)
   }
 }
 
+export {install};
 export { default as OlvyWidget } from "./components/OlvyWidget.vue";
-
+export default install;
